f83460872b5c6119864ab9980e18af9fba864436
kazukikanomata <77597098+kazukikanomata@users.noreply.github.com>
2022-09-09 19:16:53 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -55,3 +55,22 @@
 // // template文字列を用いた方法
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
+
+// アロー関数
+// ()は省略もできる
+// {}も省略できる
+const func2 = (str) => str;
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
+
+// 従来の関数
+
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
